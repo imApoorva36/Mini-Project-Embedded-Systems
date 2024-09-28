417ef573811fc821589a5adf78446de222c96096
Captaincool2017 <mike.221ec233@nitk.edu.in>
2024-09-28 22:24:21 +0530

    added client and changes to the esp32 code

--- esp32/rehab_gloves/rehab_gloves.ino
+++ esp32/rehab_gloves/rehab_gloves.ino
@@ -1,76 +1,106 @@
 #include <ESP32Servo.h>
-// Define the number of flex sensors and servos
-#define NUM_FLEX_SENSORS 5
-#define NUM_SERVOS 10
-#define change 1
-// Analog pins for the flex sensors
-int flexPins[NUM_FLEX_SENSORS] = {15, 2, 0, 4, 12}; // Change these pins according to your wiring
-int servoAngle[NUM_FLEX_SENSORS] int servoAnglePast[NUM_FLEX_SENSORS] = {0};
-// Digital pins for the servos
-int servoPins[NUM_SERVOS] = {19, 18, 5, 17, 32, 33, 25, 26, 27, 14}; // Change these pins according to your wiring
+#include <WiFi.h>
+#include <WebSocketsClient.h>
 
-// Create Servo objects
-Servo servos[NUM_SERVOS];
+const int numFingers = 5;
+const int flexSensorPin[numFingers] = {34, 35, 32, 33, 36};
+const int servoPin[numFingers][2] = { {25, 26}, {27, 14}, {12, 13}, {16, 17}, {18, 19} };
 
-// Flex sensor readings
-int flexValues[NUM_FLEX_SENSORS];
+WebSocketsClient webSocket;
+Servo servos[numFingers][2];
 
-void setup()
-{
-  // Initialize serial communication
-  Serial.begin(115200);
+float targetAngle[numFingers] = {0};
+float assistedAngle[numFingers] = {0};
+float thresholdChange = 5;
+bool assistActive[numFingers] = {false};
 
-  // Attach servos to pins
-  for (int i = 0; i < NUM_SERVOS; i++)
-  {
-    servos[i].attach(servoPins[i]);
+const char* ssid = "your_SSID";
+const char* password = "your_PASSWORD";
+
+const char* webSocketServerHost = "your.server.com";
+const int webSocketServerPort = 81;
+
+float previousAngle[numFingers] = {0};
+unsigned long lastCheckTime[numFingers] = {0};
+unsigned long checkInterval = 1000;
+
+void setupWiFi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi...");
   }
+  Serial.println("Connected to WiFi.");
 }
 
-void loop()
-{
-  // Read values from flex sensors
-  for (int i = 0; i < NUM_FLEX_SENSORS; i++)
-  {
-    flexValues[i] = analogRead(flexPins[i]);
-    Serial.print("Flex Sensor ");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(flexValues[i]);
-  }
-
-  // Map flex sensor values to servo positions
-  for (int i = 0; i < NUM_FLEX_SENSORS; i++)
-  {
-    // Map flex sensor value to a servo angle (0 to 180 degrees)
-    servoAngle = map(flexValues[i], 0, 4095, 0, 180);
-
-    // Control two servos with the same flex sensor value
-  }
-  for (int i = 0; i < NUM_FLES_SENSORS; i++)
-  {
-    if (abs(servoAngle - servoAnglePast) >= change)
-    {
-      servos[i * 2].write(servoAngle + 5);
-      servos[i * 2 + 1].write(175 - servoAngle);
-      servoAnglePast = servoAngle;
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  if (type == WStype_TEXT) {
+    String msg = String((char *)payload);
+    for (int i = 0; i < numFingers; i++) {
+      String fingerID = String(i);
+      if (msg.indexOf("targetAngle" + fingerID) >= 0) {
+        targetAngle[i] = msg.substring(msg.indexOf(":") + 1).toFloat();
+      }
+      if (msg.indexOf("assistedAngle" + fingerID) >= 0) {
+        assistedAngle[i] = msg.substring(msg.indexOf(":") + 1).toFloat();
+      }
     }
   }
-
-  // Delay for stability
-  delay(100);
 }
 
-int flex_map(int k)
-{
-  const int mapping[] = {180, 165, 150, 135, 128, 120, 113, 105, 90, 85, 80, 75, 68, 60, 55, 50, 45, 40, 35, 30, 26, 23, 19, 15, 11, 8, 4, 0};
-  int index = k - 6;
-  if (index >= 0 && index < sizeof(mapping) / sizeof(mapping[0]))
-  {
-    return mapping[index];
+int calculateServoSpeed(float currentAngle, float targetAngle) {
+  float angleDifference = targetAngle - currentAngle;
+  if (angleDifference > 0) {
+    return map(angleDifference, 0, 180, 90, 180);
+  } else if (angleDifference < 0) {
+    return map(angleDifference, -180, 0, 0, 90);
+  } else {
+    return 90;
   }
-  else
-  {
-    return 0;
+}
+
+void moveServos(int fingerIndex, float currentAngle, float targetAngle) {
+  int speed = calculateServoSpeed(currentAngle, targetAngle);
+  servos[fingerIndex][0].write(speed);
+  servos[fingerIndex][1].write(180 - speed);
+}
+
+void setup() {
+  Serial.begin(115200);
+  setupWiFi();
+  webSocket.begin(webSocketServerHost, webSocketServerPort, "/");
+  webSocket.onEvent(webSocketEvent);
+  for (int i = 0; i < numFingers; i++) {
+    servos[i][0].attach(servoPin[i][0]);
+    servos[i][1].attach(servoPin[i][1]);
+    servos[i][0].write(90);
+    servos[i][1].write(90);
+    previousAngle[i] = 0;
+    lastCheckTime[i] = millis();
   }
+}
+
+void loop() {
+  webSocket.loop();
+  for (int i = 0; i < numFingers; i++) {
+    float flexSensorVal = analogRead(flexSensorPin[i]);
+    float currentAngle = map(flexSensorVal, 0, 4095, 0, 180);
+    unsigned long currentTime = millis();
+    if (currentTime - lastCheckTime[i] >= checkInterval) {
+      if (abs(currentAngle - previousAngle[i]) >= thresholdChange) {
+        assistActive[i] = true;
+      }
+      previousAngle[i] = currentAngle;
+      lastCheckTime[i] = currentTime;
+    }
+    if (assistActive[i]) {
+      moveServos(i, currentAngle, targetAngle[i]);
+      if (currentAngle >= assistedAngle[i]) {
+        assistActive[i] = false;
+        servos[i][0].write(90);
+        servos[i][1].write(90);
+      }
+    }
+  }
+  delay(10);
 }
