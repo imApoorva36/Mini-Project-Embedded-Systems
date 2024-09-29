22180a038caa6503028e9ea5f5f049e2787cdbe8
Apoorva Agrawal <apoorva3569@gmail.com>
2024-09-29 23:54:15 +0530

    allowed hosting

--- rehabglove/rehabglove/settings.py
+++ rehabglove/rehabglove/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "mini-project-embedded-systems.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
+
 
 # Application definition
 INSTALLED_APPS = [
